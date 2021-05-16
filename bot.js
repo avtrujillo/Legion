7607ead95f70cdf15729238be6be66aa9a17dbc7
Alexander Trujillo <alexandervtrujillo@gmail.com>
2021-05-15 23:02:58 -0700

    start transitioning to typescript

--- bot.js
+++ bot.js
@@ -1,61 +1,84 @@
-const fs = require("fs");
-const Sentry = require("@sentry/node");
-Sentry.init({dsn: process.env.SENTRY_DSN });
-
-const Base = require("eris-sharder").Base;
-
-class Tupperbox extends Base {
-	constructor(bot) {
-		super(bot);
-	}
-
-	launch() {
-		let bot = this.bot;
-		bot.base = this;
-		bot.sentry = Sentry;
-		bot.db = require("./modules/db");
-		bot.msg = require("./modules/msg");
-		bot.cmd = require("./modules/cmd");
-		bot.proxy = require("./modules/proxy");
-		bot.paginator = require("./modules/paginator");
-		bot.recent = {};
-		bot.cmds = {};
-		bot.dialogs = {};
-		bot.owner = process.env.DISCORD_OWNERID;
-		bot.defaultCfg = { prefix: process.env.DEFAULT_PREFIX, lang: process.env.DEFAULT_LANG };
-		try { bot.blacklist = require("./modules/blacklist.json"); }
-		catch(e) { bot.blacklist = []; }
-		require("./modules/ipc")(bot);
-		require("./modules/util")(bot);
-
-		let files = fs.readdirSync("./commands");
-		files.forEach(file => {
-			bot.cmds[file.slice(0,-3)] = require("./commands/"+file);
-		});
-
-		files = fs.readdirSync("./events");
-		files.forEach(file => {
-			bot.on(file.slice(0,-3), (...args) => require("./events/"+file)(...args,bot));
-		});
-
-		process.on("message", message => {
-			if(bot.ipc[message.name]) bot.ipc[message.name](message);
-		});
-
-		setInterval(() => bot.updateStatus(),3600000); //every hour
-		bot.updateStatus();
-
-		if (!process.env.BOT_INVITE)
-			delete bot.cmds.invite;
-
-		if (!process.env.SUPPORT_INVITE)
-			delete bot.cmds.feedback;
-
-		if(!fs.existsSync("privacy.txt")) {
-			console.warn("no privacy command");
-			delete bot.cmds.privacy;
-		}
-	}
-}
-
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
+var fs = require("fs");
+var Sentry = require("@sentry/node");
+Sentry.init({ dsn: process.env.SENTRY_DSN });
+var Base = require("eris-sharder").Base;
+var Tupperbox = (function (_super) {
+    __extends(Tupperbox, _super);
+    function Tupperbox(bot) {
+        return _super.call(this, bot) || this;
+    }
+    Tupperbox.prototype.launch = function () {
+        var bot = this.bot;
+        bot.base = this;
+        bot.sentry = Sentry;
+        bot.db = require("./modules/db");
+        bot.msg = require("./modules/msg");
+        bot.cmd = require("./modules/cmd");
+        bot.proxy = require("./modules/proxy");
+        bot.paginator = require("./modules/paginator");
+        bot.recent = {};
+        bot.cmds = {};
+        bot.dialogs = {};
+        bot.owner = process.env.DISCORD_OWNERID;
+        bot.defaultCfg = { prefix: process.env.DEFAULT_PREFIX, lang: process.env.DEFAULT_LANG };
+        try {
+            bot.blacklist = require("./modules/blacklist.json");
+        }
+        catch (e) {
+            bot.blacklist = [];
+        }
+        require("./modules/ipc")(bot);
+        require("./modules/util")(bot);
+        var files = fs.readdirSync("./commands");
+        files.forEach(function (file) {
+            bot.cmds[file.slice(0, -3)] = require("./commands/" + file);
+        });
+        files = fs.readdirSync("./events");
+        files.forEach(function (file) {
+            bot.on(file.slice(0, -3), function () {
+                var args = [];
+                for (var _i = 0; _i < arguments.length; _i++) {
+                    args[_i] = arguments[_i];
+                }
+                return require("./events/" + file).apply(void 0, __spreadArrays(args, [bot]));
+            });
+        });
+        process.on("message", function (message) {
+            if (bot.ipc[message.name])
+                bot.ipc[message.name](message);
+        });
+        setInterval(function () { return bot.updateStatus(); }, 3600000);
+        bot.updateStatus();
+        if (!process.env.BOT_INVITE)
+            delete bot.cmds.invite;
+        if (!process.env.SUPPORT_INVITE)
+            delete bot.cmds.feedback;
+        if (!fs.existsSync("privacy.txt")) {
+            console.warn("no privacy command");
+            delete bot.cmds.privacy;
+        }
+    };
+    return Tupperbox;
+}(Base));
 module.exports = Tupperbox;
+//# sourceMappingURL=bot.js.map
--- index.js
+++ index.js
@@ -1,67 +1,110 @@
-require("dotenv").config();
-const Sharder = require("eris-sharder").Master;
-const cluster = require("cluster");
-
-const init = async () => {
-	if(cluster.isMaster) {
-		try { 
-			require("./auth.json");
-			throw new Error("outdated");
-		} catch(e) { 
-			if(e.message == "outdated") throw new Error("auth.json is outdated, please use the .env file instead! See the github page for more info");
-		}
-
-		await require("./modules/db").init();
-	}
-
-	let sharder = new Sharder("Bot " + process.env.DISCORD_TOKEN,"/bot.js",{
-		clientOptions: {
-			disableEvents: {
-				GUILD_BAN_ADD: true,
-				GUILD_BAN_REMOVE: true,
-				MESSAGE_DELETE: true,
-				MESSAGE_DELETE_BULK: true,
-				TYPING_START: true,
-				VOICE_STATE_UPDATE: true
-			},
-			messageLimit: 0,
-			guildSubscriptions: false,
-			allowedMentions: { roles: false },
-			restMode: true,
-			ratelimiterOffset: 5,
-			intents: [
-				"guilds",
-				"guildMessages",
-				"guildMessageReactions",
-				"directMessages",
-				"directMessageReactions"
-			],
-			maxConcurrency: "auto"
-		},
-		stats: true,
-		debug: true,
-		shards: +process.env.SHARDS,
-		clusters: +process.env.CLUSTERS || process.env.DEV? 1 : undefined,
-		name: "Tupperbox",
-		clusterTimeout: 0.1
-	});
-
-	sharder.eris.on("debug",console.log);
-	
-
-	if(cluster.isMaster) {
-		let events = require("./modules/ipc.js");
-
-		cluster.on("message",(worker,message) => {
-			if(message.name == "reloadIPC") {
-				delete require.cache[require.resolve("./modules/ipc.js")];
-				events = require("./modules/ipc.js");
-				console.log("Reloaded IPC plugin!");
-			} else if(events[message.name]) {
-				events[message.name](worker,message,sharder);
-			}
-		});
-	}
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
 };
-
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (_) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
+var _this = this;
+require("dotenv").config();
+var Sharder = require("eris-sharder").Master;
+var cluster = require("cluster");
+var init = function () { return __awaiter(_this, void 0, void 0, function () {
+    var sharder, events_1;
+    return __generator(this, function (_a) {
+        switch (_a.label) {
+            case 0:
+                if (!cluster.isMaster) return [3, 2];
+                try {
+                    require("./auth.json");
+                    throw new Error("outdated");
+                }
+                catch (e) {
+                    if (e.message == "outdated")
+                        throw new Error("auth.json is outdated, please use the .env file instead! See the github page for more info");
+                }
+                return [4, require("./modules/db").init()];
+            case 1:
+                _a.sent();
+                _a.label = 2;
+            case 2:
+                sharder = new Sharder("Bot " + process.env.DISCORD_TOKEN, "/bot.js", {
+                    clientOptions: {
+                        disableEvents: {
+                            GUILD_BAN_ADD: true,
+                            GUILD_BAN_REMOVE: true,
+                            MESSAGE_DELETE: true,
+                            MESSAGE_DELETE_BULK: true,
+                            TYPING_START: true,
+                            VOICE_STATE_UPDATE: true
+                        },
+                        messageLimit: 0,
+                        guildSubscriptions: false,
+                        allowedMentions: { roles: false },
+                        restMode: true,
+                        ratelimiterOffset: 5,
+                        intents: [
+                            "guilds",
+                            "guildMessages",
+                            "guildMessageReactions",
+                            "directMessages",
+                            "directMessageReactions"
+                        ],
+                        maxConcurrency: "auto"
+                    },
+                    stats: true,
+                    debug: true,
+                    shards: +process.env.SHARDS,
+                    clusters: +process.env.CLUSTERS || process.env.DEV ? 1 : undefined,
+                    name: "Tupperbox",
+                    clusterTimeout: 0.1
+                });
+                sharder.eris.on("debug", console.log);
+                if (cluster.isMaster) {
+                    events_1 = require("./modules/ipc.js");
+                    cluster.on("message", function (worker, message) {
+                        if (message.name == "reloadIPC") {
+                            delete require.cache[require.resolve("./modules/ipc.js")];
+                            events_1 = require("./modules/ipc.js");
+                            console.log("Reloaded IPC plugin!");
+                        }
+                        else if (events_1[message.name]) {
+                            events_1[message.name](worker, message, sharder);
+                        }
+                    });
+                }
+                return [2];
+        }
+    });
+}); };
 init();
+//# sourceMappingURL=index.js.map
